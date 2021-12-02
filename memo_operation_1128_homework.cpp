a82ca36d55fc7d7f6f6d8dae76afffb1d508bb78
leeyuanli <leeyuanli@tencent.com>
2021-12-02 17:34:32 +0800

    add note

--- memo_operation_1128_homework.cpp
+++ memo_operation_1128_homework.cpp
@@ -6,12 +6,14 @@ using namespace std;
 int main()
 {
 	// Quesetion one printf first address
+	// “hello” 字符常量返回的就是首地址，所以可以赋值给指针变量
 	char* str = NULL;
 	str = (char*)malloc(sizeof(char*)*14);
 	strncpy(str, "Hello,World.", 13);
 	printf("Address of First Character is %p", str);
 	
 	// Quesetion two using point chage the value of last character
+	// "" 长字符，末尾包含/0 ‘’就是一个char
 	*(str+11) = '!';
 	
 	// Quesetion three malloc 1024 bytes memeory and free memory

d03b1168fc413b9070b65269e819b518f832b756
leeyuanli <leeyuanli@tencent.com>
2021-12-02 17:32:05 +0800

    add info

--- memo_operation_1128_homework.cpp
+++ memo_operation_1128_homework.cpp
@@ -5,14 +5,16 @@ using namespace std;
 
 int main()
 {
+	// Quesetion one printf first address
 	char* str = NULL;
 	str = (char*)malloc(sizeof(char*)*14);
 	strncpy(str, "Hello,World.", 13);
 	printf("Address of First Character is %p", str);
 	
-	
+	// Quesetion two using point chage the value of last character
 	*(str+11) = '!';
 	
+	// Quesetion three malloc 1024 bytes memeory and free memory
 	char* big_str = NULL;
 	big_str = (char*)malloc(sizeof(char*)*1024);
 	char* p = str;
